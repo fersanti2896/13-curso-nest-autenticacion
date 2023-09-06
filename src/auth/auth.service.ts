f7b8b79340eb2f6cf546386f9b86294428415859
Fernando Nicolás <fersanti2896@gmail.com>
2023-09-06 12:45:30 -0600

    Login de usuario.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 import { User } from './entities/user.entity';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -13,15 +14,36 @@ export class AuthService {
 
   async create( createUserDto: CreateUserDto ) {
     try {
-      const user = this.userRepository.create( createUserDto );
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({ 
+        ...userData,
+        password: bcrypt.hashSync( password, 10 )
+      });
 
       await this.userRepository.save( user );
+      delete user.password;
 
       return user;
     } catch (error) {
       console.log(error);
       this.handleDBErrors( error );
     }
+  }
+
+  async login( loginUserDto: LoginUserDto ) {
+    const { email, password } = loginUserDto;
+
+    const user = await this.userRepository.findOne({ 
+      where: { email },
+      select: { email: true, password: true }
+    });
+
+    if( !user ) throw new UnauthorizedException('Credenciales no válidas.')
+
+    if( !bcrypt.compareSync( password, user.password ) ) throw new UnauthorizedException('Credenciales no válidas.')
+
+    return user;
   }
 
   private handleDBErrors( error: any ): never {

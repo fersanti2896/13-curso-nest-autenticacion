0cc4206d4a2de562d17f1eb3579b9ef48b9cf6ce
Fernando Nicolás <fersanti2896@gmail.com>
2023-09-06 22:48:08 -0600

    Implementando Custom Decorators.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, Body, Get, UseGuards} from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
+import { User } from './entities/user.entity';
+import { GetUser, RawHeaders } from './decorators';
 
 @Controller('auth')
 export class AuthController {
@@ -19,10 +21,18 @@ export class AuthController {
 
   @Get('private')
   @UseGuards( AuthGuard( ) )
-  testingPrivateRoute() {
+  testingPrivateRoute(
+    @Req() request: Express.Request,
+    @GetUser() user: User, 
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[]
+  ) {
     return {
       ok: true,
-      messsage: 'Hola mundo private'
+      messsage: 'Hola mundo private',
+      user, 
+      userEmail,
+      rawHeaders
     }
   }
 }

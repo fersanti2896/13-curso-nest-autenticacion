9e281558a7da4f1dfc7ef8f0a939e3169e5d20c6
Fernando Nicolás <fersanti2896@gmail.com>
2023-09-07 20:50:04 -0600

    Modificando SEED con usuarios, productos e imagenes.

--- src/seed/data/seed-data.ts
+++ src/seed/data/seed-data.ts
@@ -15,11 +15,39 @@ interface SeedProduct {
 type ValidSizes = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
 type ValidTypes = 'shirts'|'pants'|'hoodies'|'hats';
 
+interface SeedUser {
+    email: string;
+    fullName: string;
+    password: string;
+    roles: string[];
+}
+
 interface SeedData {
+    users: SeedUser[];
     products: SeedProduct[];
 }
 
 export const initialData: SeedData = {
+    users: [ 
+        {
+            email: 'test1@gamil.com',
+            fullName: 'Test One',
+            password: 'Un1fac3;',
+            roles: ['admin']
+        },
+        {
+            email: 'test2@gamil.com',
+            fullName: 'Test Two',
+            password: 'Un1fac3;',
+            roles: ['admin', 'user']
+        },
+        {
+            email: 'test3@gamil.com',
+            fullName: 'Test Three',
+            password: 'Un1fac3;',
+            roles: ['user', 'super-user']
+        }
+     ],
     products: [
         {
             description: "Introducing the Tesla Chill Collection. The Men’s Chill Crew Neck Sweatshirt has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The sweatshirt features a subtle thermoplastic polyurethane T logo on the chest and a Tesla wordmark below the back collar. Made from 60% cotton and 40% recycled polyester.",
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,28 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { ProductsService } from './../products/products.service';
 import { initialData } from './data/seed-data';
+import { User } from '../auth/entities/user.entity';
 
 @Injectable()
 export class SeedService {
   constructor(
-    private readonly productsService: ProductsService
+    private readonly productsService: ProductsService,
+    @InjectRepository( User )
+    private readonly userRepository: Repository<User>
   ) {}
 
   async runSeed() {
-    await this.insertNewProducts();
+    await this.deleteTables();
+    const adminUser = await this.insertUsers();
+    await this.insertNewProducts( adminUser );
 
     return 'SEED EXECUTED'
   }
 
-  private async insertNewProducts() {
+  /* Insertar usuarios */
+  private async insertUsers() {
+    const seedUsers = initialData.users;
+    const users: User[] = [];
+
+    seedUsers.forEach( user => {
+      users.push( this.userRepository.create( user ) );
+    } );
+
+    const dbUsers = await this.userRepository.save( seedUsers );
+
+    return dbUsers[0];
+  }
+
+  /* Purga las todas las tablas en BD */
+  private async deleteTables() {
+    await this.productsService.deleteAllProducts();
+
+    const queryBuilder = this.userRepository.createQueryBuilder();
+    await queryBuilder.delete().where({}).execute();
+  }
+
+  private async insertNewProducts( user: User ) {
     await this.productsService.deleteAllProducts();
 
     const seedProducts = initialData.products;
     const insertPromises = [];
 
-    // seedProducts.forEach( product => {
-    //   insertPromises.push( this.productsService.create(product) );
-    // } );
+    seedProducts.forEach( product => {
+      insertPromises.push( this.productsService.create(product, user) );
+    } );
 
     await Promise.all( insertPromises );
 
